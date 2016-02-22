649046725feda8a630634442eccb18d37d40c1e2
James Green <dups@me.com>
2016-02-22 13:59:46 +0000

    Add randomisation.

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 // An interactive terminal implementation of Conway's Game of Life.
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Grid struct {
 	cells         [][]bool
@@ -9,13 +13,11 @@ type Grid struct {
 }
 
 func main() {
-	//g := NewGrid(4, 6)
-	//fmt.Println(g)
-	i := -12
-	w := 5
-	i += w
-	i %= w
-	fmt.Println(i)
+	g := NewGrid(4, 6)
+	rand.Seed(time.Now().UnixNano())
+	g.Randomise()
+	fmt.Println(g)
+
 }
 
 func NewGrid(w, h int) *Grid {
@@ -31,8 +33,12 @@ func (g *Grid) State(x, y int) bool {
 	return g.cells[x][y]
 }
 
-func (g *Grid) toroidal(x, y int) (tx, ty int) {
-	//TODO: this
+func (g *Grid) Randomise() {
+	for x := 0; x < g.width; x++ {
+		for y := 0; y < g.height; y++ {
+			g.cells[x][y] = rand.Intn(2) == 0
+		}
+	}
 }
 
 func (g *Grid) aliveNeighbours(x, y int) int {
@@ -40,10 +46,22 @@ func (g *Grid) aliveNeighbours(x, y int) int {
 	for dx := range []int{-1, 0, 1} {
 		for dy := range []int{-1, 0, 1} {
 			nx, ny := g.toroidal(x+dx, y+dy)
-			if (dx != 0 || dy != 0) && g.cells[nx][ny] {
+			if (nx != x || ny != y) && g.cells[nx][ny] {
 				aliveNeighbours++
 			}
 		}
 	}
 	return aliveNeighbours
+}
+
+func (g *Grid) toroidal(x, y int) (tx, ty int) {
+	tx = x % g.width
+	ty = y % g.height
+	if tx < 0 {
+		tx += g.width
+	}
+	if ty < 0 {
+		tx += g.height
+	}
+	return
 }
